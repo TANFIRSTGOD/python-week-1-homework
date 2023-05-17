1d112be55aa3c4422c21b9a8f6e6be914dfdf7e0
tan05 <tan.tantham.tan@gmail.com>
2023-05-17 14:14:42 +0700

    commit 6 fix

--- 6.py
+++ 6.py
@@ -1,6 +1,16 @@
-from math import lcm
+def lcm(x,y):
+    x_multiplyer = 1
+    y_multiplyer = 1
+    x_multiples = []
+    y_multiples = []
+    while True:
+        x_multiples.append(x * x_multiplyer)
+        x_multiplyer += 1
+        y_multiples.append(y * y_multiplyer)
+        y_multiplyer += 1
+        for i in x_multiples:
+            for v in y_multiples:
+                if i == v:
+                    return(v)
 
-def lcm_calculate(x,y):
-    return lcm(x,y)
-
-print(lcm_calculate(1,3))
+print(lcm(24,36))
